cf1248427939e68e51e585c67213a8353a8fbf80
fallGamlet <fallgamlet@yandex.ru>
2017-03-22 03:24:14 +0200

    remove deprecated fields

--- app/src/main/java/com/fallgamlet/dnestrcinema/network/DataSettings.java
+++ app/src/main/java/com/fallgamlet/dnestrcinema/network/DataSettings.java
@@ -8,8 +8,5 @@ public class DataSettings {
     public static String BASE_URL = "http://kinotir.md";
     public static String PATH_RSS = "/rss";
     public static String PATH_MOVE = "/movie/";
-    public static String PATH_IMG_ROOM_BLUE = "/UserFiles/Image/blue.gif";
-    public static String PATH_IMG_ROOM_BORDO = "/UserFiles/Image/bordo.gif";
-    public static String PATH_IMG_ROOM_DVD = "/UserFiles/Image/dvd.gif";
     public static String PATH_IMG_SCHEME = "/UserFiles/Image/scheme.jpg";
 }
