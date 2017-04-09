0460c6ab5ac888a8945c5d647837a1778a12e5ad
fallGamlet <fallgamlet@yandex.ru>
2017-04-10 01:03:51 +0300

    Create MVP interfaces

--- app/src/main/java/com/fallgamlet/dnestrcinema/mvp/presenters/IPresenter.java
+++ app/src/main/java/com/fallgamlet/dnestrcinema/mvp/presenters/IPresenter.java
@@ -1,8 +1,18 @@
 package com.fallgamlet.dnestrcinema.mvp.presenters;
 
+import android.os.Bundle;
+
+import com.fallgamlet.dnestrcinema.mvp.views.IView;
+
 /**
  * Created by fallgamlet on 09.04.17.
  */
 
-public interface IPresenter {
+public interface IPresenter <T extends IView> {
+    void setView(T view);
+    T getView();
+    void onStart();
+    void onStop();
+    void onPause(Bundle bundle);
+    void onResume(Bundle bundle);
 }
--- app/src/main/java/com/fallgamlet/dnestrcinema/mvp/views/IView.java
+++ app/src/main/java/com/fallgamlet/dnestrcinema/mvp/views/IView.java
@@ -1,8 +1,12 @@
 package com.fallgamlet.dnestrcinema.mvp.views;
 
+import android.content.Context;
+import com.fallgamlet.dnestrcinema.mvp.presenters.IPresenter;
+
 /**
  * Created by fallgamlet on 09.04.17.
  */
 
-public interface BaseView {
+public interface IView <T extends IPresenter> {
+    Context getContext();
 }
