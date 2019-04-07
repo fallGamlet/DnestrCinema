7703c1e2de1e60caf2360c99ae7a5fb4c14aab44
fallGamlet <fallgamlet@yandex.ru>
2019-04-07 22:53:44 +0300

    update libs

---
+++ app/src/main/java/com/fallgamlet/dnestrcinema/app/SimpleAppGlideModule.java
@@ -0,0 +1,8 @@
+package com.fallgamlet.dnestrcinema.app;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class SimpleAppGlideModule extends AppGlideModule {
+}
