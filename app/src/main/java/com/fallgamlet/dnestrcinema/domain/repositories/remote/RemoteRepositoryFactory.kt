907ecff1d60fe9ead8179384de0d00c310f145f6
fallGamlet <fallgamlet@yandex.ru>
2025-04-30 22:21:16 +0300

    remove RemoteRepositoryFactory as unused

--- app/src/main/java/com/fallgamlet/dnestrcinema/domain/repositories/remote/RemoteRepositoryFactory.kt
+++
@@ -1,6 +0,0 @@
-package com.fallgamlet.dnestrcinema.domain.repositories.remote
-
-interface RemoteRepositoryFactory {
-    fun filmRepository(): FilmRepository
-    fun newsRepository(): NewsRepository
-}
