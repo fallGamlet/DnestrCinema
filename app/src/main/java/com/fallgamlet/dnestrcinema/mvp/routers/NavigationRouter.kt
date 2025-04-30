31a5eac725be5e68fd2c47ef59e2a9ef72c2e1c3
fallGamlet <fallgamlet@yandex.ru>
2025-05-01 02:59:41 +0300

    make usage jetpack compose bottom navigation instead of old custom

--- app/src/main/java/com/fallgamlet/dnestrcinema/mvp/routers/NavigationRouter.kt
+++ app/src/main/java/com/fallgamlet/dnestrcinema/mvp/routers/NavigationRouter.kt
@@ -2,13 +2,5 @@ package com.fallgamlet.dnestrcinema.mvp.routers
 
 
 interface NavigationRouter {
-    fun showToday()
-
-    fun showSoon()
-
-    fun showAbout()
-
-    fun showNews()
-
     fun showMovieDetail(movieLink: String)
 }
--- app/src/main/java/com/fallgamlet/dnestrcinema/ui/start/StartActivity.kt
+++ app/src/main/java/com/fallgamlet/dnestrcinema/ui/start/StartActivity.kt
@@ -2,108 +2,50 @@ package com.fallgamlet.dnestrcinema.ui.start
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.fallgamlet.dnestrcinema.R
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.ViewModelProvider
 import com.fallgamlet.dnestrcinema.app.AppFacade.Companion.instance
 import com.fallgamlet.dnestrcinema.dagger.getAppComponent
-import com.fallgamlet.dnestrcinema.domain.models.NavigationItem
-import com.fallgamlet.dnestrcinema.mvp.presenters.MvpNavigationPresenter
 import com.fallgamlet.dnestrcinema.mvp.routers.NavigationRouter
-import com.fallgamlet.dnestrcinema.mvp.views.MvpNavigationView
 import com.fallgamlet.dnestrcinema.ui.movie.detail.MovieDetailActivity
-import com.fallgamlet.dnestrcinema.ui.navigation.MvpBottomNavigationView
-import com.fallgamlet.dnestrcinema.ui.navigation.MvpNavigationPresenterImpl
-import com.fallgamlet.dnestrcinema.utils.ViewUtils.shareApp
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import javax.inject.Inject
 
 class StartActivity : AppCompatActivity(), NavigationRouter {
-    private lateinit var mViewPager: ViewPager2
-    private lateinit var mBottomNavigationView: BottomNavigationView
-    private lateinit var adapter: ViewPagerAdapter
-    private lateinit var bottomNavigationPresenter: MvpNavigationPresenter
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         getAppComponent().inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.start)
-        mViewPager = findViewById(R.id.viewpager)
-        mBottomNavigationView = findViewById(R.id.bottomNavigationView)
-        initData()
-    }
-
-    private fun initData() {
-        initNavigation()
-        setupViewPager(mViewPager)
-        showToday()
-    }
-
-    private fun initNavigation() {
-        val bottomNavigationView: MvpNavigationView = MvpBottomNavigationView(mBottomNavigationView)
-        bottomNavigationPresenter = MvpNavigationPresenterImpl(bottomNavigationView, this)
-        bottomNavigationView.presenter = bottomNavigationPresenter
-    }
-
-    private fun setupViewPager(viewPager: ViewPager2) {
-        initViewPagerAdapter()
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                handlePageSelection(position)
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    AppScreen(
+                        viewModelFactory = viewModelFactory,
+                        context = { this@StartActivity }
+                    )
+                }
             }
-        })
-        viewPager.adapter = adapter
-    }
-
-    private fun initViewPagerAdapter() {
-        adapter = ViewPagerAdapter(
-            fragmentManager = supportFragmentManager,
-            lifecycle = lifecycle,
-            getCount = { instance.navigations.size },
-            createPage = ::createFragment
         )
     }
 
-    private fun createFragment(position: Int): Fragment {
-        val fragmentFactory = instance.fragmentFactory
-            ?: throw IllegalArgumentException("fragment factory must be initialized")
-        val id = instance.navigations.getOrNull(position)
-        return when (id) {
-            NavigationItem.NavigationId.TODAY -> fragmentFactory.createTodayView()
-            NavigationItem.NavigationId.SOON -> fragmentFactory.createSoonView()
-            NavigationItem.NavigationId.NEWS -> fragmentFactory.createNewsView()
-            NavigationItem.NavigationId.ABOUT -> fragmentFactory.createAboutView()
-            else -> throw IllegalArgumentException("Not implemented page (position is $position, id is $id)")
-        }
-    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
 
-    private fun handlePageSelection(position: Int) {
-        val id = instance.navigations.getOrNull(position)
-        when (id) {
-            NavigationItem.NavigationId.TODAY -> showToday()
-            NavigationItem.NavigationId.SOON -> showSoon()
-            NavigationItem.NavigationId.NEWS -> showNews()
-            NavigationItem.NavigationId.ABOUT -> showAbout()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.actionShareApp -> {
-                shareApp(this)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        return when(item.itemId) {
+//            R.id.actionShareApp -> {
+//                shareApp(this)
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 
     override fun onPause() {
         instance.navigationRouter = null
@@ -113,40 +55,6 @@ class StartActivity : AppCompatActivity(), NavigationRouter {
     override fun onResume() {
         super.onResume()
         instance.navigationRouter = this
-    }
-
-    override fun showToday() {
-        showViewWithNavigationId(NavigationItem.NavigationId.TODAY)
-        bottomNavigationPresenter.onTodaySelected()
-    }
-
-    override fun showSoon() {
-        showViewWithNavigationId(NavigationItem.NavigationId.SOON)
-        bottomNavigationPresenter.onSoonSelected()
-    }
-
-    override fun showAbout() {
-        showViewWithNavigationId(NavigationItem.NavigationId.ABOUT)
-        bottomNavigationPresenter.onAboutSelected()
-    }
-
-    override fun showNews() {
-        showViewWithNavigationId(NavigationItem.NavigationId.NEWS)
-        bottomNavigationPresenter.onNewsSelected()
-    }
-
-    private fun showViewWithNavigationId(navigationId: Int) {
-        val pos = instance.navigations.indexOf(navigationId)
-        showViewWithPosition(pos)
-    }
-
-    private fun showViewWithPosition(position: Int) {
-        if (position < 0 || position >= adapter.itemCount)
-            return
-
-        if (mViewPager.currentItem != position) {
-            mViewPager.currentItem = position
-        }
     }
 
     override fun showMovieDetail(movieLink: String) {
