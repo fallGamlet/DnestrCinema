7937d6b26a2180454b651e5276070e66da23a18e
fallGamlet <fallgamlet@yandex.ru>
2025-05-10 03:57:09 +0300

    migrate to gradle kts for top level build.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://clojars.org/repo/")
+        maven("https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://clojars.org/repo/")
+        maven("https://jitpack.io")
+    }
+}
+
 rootProject.name = "Dnestr Cinema"
 include(":app")
 include(":KinotirApi")
